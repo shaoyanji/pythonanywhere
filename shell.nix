8337e03e0025e4dad7f375d482f23f28d6955918
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-01-27 15:22:21 +0100

    requirements file due to dev container codespace

--- shell.nix
+++ shell.nix
@@ -2,26 +2,31 @@
 #   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
 #   pkgs = import nixpkgs { config = {}; overlays = []; };
 # in
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell
-# pkgs.mkShellNoCC 
+{pkgs ? import <nixpkgs> {}}:
+pkgs.mkShell
+# pkgs.mkShellNoCC
 {
   nativeBuildInputs = with pkgs; [
   ];
   buildInputs = with pkgs; [
   ];
   packages = with pkgs; [
-    (python310.withPackages (ps: with ps; [
-      flask
-      fuzzywuzzy
-      markdown2
-      #python-dotenv
-    ]))
+    (python310.withPackages (ps:
+      with ps; [
+        flask
+        fuzzywuzzy
+        markdown2
+        #python-dotenv
+      ]))
     yq-go
     go-task
     fzf
   ];
-   shellHook = /*bash*/ ''
-    task
-   '';
+  shellHook =
+    /*
+    bash
+    */
+    ''
+      task
+    '';
 }
