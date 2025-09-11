9542baa51dde4c12888fd9f5a60ba1de572765b3
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-09-11 11:05:08 +0200

    redo

--- app/__init__.py
+++ app/__init__.py
@@ -59,7 +59,7 @@ def protected():
     ]
     result = subprocess.run(command, check=True, text=True, capture_output=True)
     if result.returncode == 0:
-        subprocess.run(["pa", "webappreload"], text=True, capture_output=True)
+        subprocess.run(["pa", "webapp", "reload"], text=True, capture_output=True)
         return jsonify(message="successful")
     else:
         return jsonify(message=f"Hello {user}")
