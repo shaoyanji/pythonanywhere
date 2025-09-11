40d16cf222830d63067db517441862eeddfc437e
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-09-11 11:22:25 +0200

    removed git

--- app/__init__.py
+++ app/__init__.py
@@ -51,18 +51,18 @@ def protected():
             401,
             {"WWW-Authenticate": 'Basic realm="API"'},
         )
-    command = [
-        "git",
-        "-C",
-        "/home/jisifu/pythonanywhere",
-        "pull",
-    ]
-    result = subprocess.run(command, check=True, text=True, capture_output=True)
-    if result.returncode == 0:
-        subprocess.run(["pa", "webapp", "reload"], text=True, capture_output=True)
-        return jsonify(message="successful")
-    else:
-        return jsonify(message=f"Hello {user}")
+    # command = [
+    #     "git",
+    #     "-C",
+    #     "/home/jisifu/pythonanywhere",
+    #     "pull",
+    # ]
+    # result = subprocess.run(command, check=True, text=True, capture_output=True)
+    # if result.returncode == 0:
+    subprocess.run(["pa", "webapp", "reload"], text=True, capture_output=True)
+    # return jsonify(message="successful")
+    # else:
+    return jsonify(message=f"Hello {user}")
 
 
 @app.route("/api/messages", methods=["GET"])
