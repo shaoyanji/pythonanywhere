019d0e8c3336cb72f23fd2fc1bbd502513b1687c
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-09-11 11:28:40 +0200

    testing

--- app/__init__.py
+++ app/__init__.py
@@ -59,10 +59,11 @@ def protected():
     ]
     subprocess.run(command, check=True, text=True, capture_output=True)
     # if result.returncode == 0:
+    subprocess.run(["touch", "test"], text=True, capture_output=True)
     subprocess.run(["doit", "webappreload"], text=True, capture_output=True)
     # return jsonify(message="successful")
     # else:
-    return jsonify(message=f"Hello {user}")
+    # return jsonify(message=f"Hello {user}")
 
 
 @app.route("/api/messages", methods=["GET"])
