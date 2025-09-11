0860ebd2612065076a94976d84fef3f5c1544e5c
Shao-yan (Matt) Ji <100967396+shaoyanji@users.noreply.github.com>
2025-09-11 11:25:28 +0200

    added with doit

--- app/__init__.py
+++ app/__init__.py
@@ -59,7 +59,7 @@ def protected():
     ]
     subprocess.run(command, check=True, text=True, capture_output=True)
     # if result.returncode == 0:
-    # subprocess.run(["pa", "webapp", "reload"], text=True, capture_output=True)
+    subprocess.run(["doit", "webappreload"], text=True, capture_output=True)
     # return jsonify(message="successful")
     # else:
     return jsonify(message=f"Hello {user}")
