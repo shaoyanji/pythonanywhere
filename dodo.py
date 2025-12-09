1df479c5abf7d07d942ed1f5a00fb8e71f8920e3
Shaoyan Ji <100967396+shaoyanji@users.noreply.github.com>
2025-12-09 13:03:13 +0000

    doitupdate

--- dodo.py
+++ dodo.py
@@ -1,46 +1,55 @@
 from doit.action import CmdAction
 
 
-def task_m():
-    """doit $(doit list | fzf | awk '{print $1}'"""
-
-    def create_cmd_string():
-        return "echo doit list "
-
+def task_createdotenv():
+    """load the dotenv"""
     return {
-        "actions": [CmdAction(create_cmd_string)],
+        "actions": ['age -d -i ~/.ssh/id_ed25519 %(dependencies)s > .env'], 
+        "targets" : [".env"],
+        "file_dep": [".env.age"],
         "verbosity": 2,
     }
-
 
 def task_webappreload():
     """pythonanywhere reload cmd"""
 
-    def create_cmd_string():
-        return (
-            "age -d -i ~/.ssh/id_ed25519 .env.age > .env && pa webapp reload && rm .env"
-        )
-
     return {
-        "actions": [CmdAction(create_cmd_string)],
+        "actions": ["pa webapp reload"],
+        "file_dep":[".env"],
         "verbosity": 2,
     }
 
+ 
 
-def task_dotenv():
+def task_gitbackup():
+    """backs everything up one github shell execution"""
+
+    return {
+        "actions": ["git add .", "git commit -m doitupdate", "git push"],
+        "verbosity": 2,
+    }
+
+def task_destroydotenv():
     """load the dotenv"""
-
-    def create_cmd_string():
-        return (
-            "age -d -i ~/.ssh/id_ed25519 .env.age > .env"
-        )
-
     return {
-        "actions": [CmdAction(create_cmd_string)],
+        "actions": ['rm .env'], 
         "verbosity": 2,
     }
 
+def task_exit():
+    """get out"""
 
+    return {
+        "actions": ["exit"],
+        "verbosity": 2,
+    }
+def task_m():
+    """doit $(doit list | fzf | awk '{print $1}'"""
+    return {
+        "actions": ["doit list"],
+        "verbosity": 2,
+        "uptodate": [True]
+    }
 def task_hello():
     """it writes hello in a hello.txt in the git repo"""
 
@@ -51,41 +60,15 @@ def task_hello():
     return {
         "actions": [python_hello],
         "targets": ["hello.txt"],
-    }
-
-
-def task_gitbackup():
-    """backs everything up one github shell execution"""
-
-    def create_cmd_string():
-        return "git add . && git commit -m doitupdate && git push -u"
-
-    return {
-        "actions": [CmdAction(create_cmd_string)],
-        "verbosity": 2,
-    }
-
-
-def task_exit():
-    """get out"""
-
-    def create_cmd_string():
-        return "exit"
-
-    return {
-        "actions": [CmdAction(create_cmd_string)],
-        "verbosity": 2,
+        "uptodate": [True]
     }
 
 
 def task_llm():
     """export environment and then groq"""
-
-    def create_cmd_string():
-        return "export $(age -d -i ~/.ssh/id_ed25519 .env.age) && tgpt -i --provider groq --key $GROQ_API_KEY --model openai/gpt-oss-20b"
-
     return {
-        "actions": [CmdAction(create_cmd_string)],
+        "actions": ["export $(age -d -i ~/.ssh/id_ed25519 .env.age) && tgpt -i --provider groq --key $GROQ_API_KEY --model openai/gpt-oss-20b"],
+        "file_dep": [".env.age"],
         "verbosity": 2,
     }
 
@@ -93,23 +76,24 @@ def task_llm():
 def task_tailwind():
     """tailwind install with npm"""
 
-    def create_cmd_string():
-        return "which npm"
-
-    #        return "npm install tailwindcss @tailwindcss/cli && echo '@import "tailwindcss";' > ./static/input.css && npx @tailwindcss/cli -i ./static/input.css -o ./static/output.css --watch"
     return {
-        "actions": [CmdAction(create_cmd_string)],
+        "actions": ["npm install tailwindcss @tailwindcss/cli",
+            "echo `@import 'tailwindcss';` > ./app/static/input.css","npx @tailwindcss/cli -i ./app/static/input.css -o ./app/static/output.css --watch"],
+        "targets": ["./app/static/input.css","./app/static/output.css"],
         "verbosity": 2,
+        "uptodate" : [True] 
     }
 
 
 def task_pythondeps():
     """python deps install"""
-
     def create_cmd_string():
         return "pip install -r requirements.txt"
 
     return {
         "actions": [CmdAction(create_cmd_string)],
+        "targets": ["requirements.txt"],
         "verbosity": 2,
+        "uptodate": [True]
     }
+   
